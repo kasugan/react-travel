62a5db782520f25bcef075265ca20eb18eebb6c0
sora <575860117@qq.com>
2021-08-30 15:31:54 +0800

    add_react_redux

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -13,52 +13,58 @@ import { productList1, productList2, productList3 } from "./mockups";
 import sideImage from "../../assets/images/sider_2019_12-09.png";
 import sideImage2 from "../../assets/images/sider_2019_02-04.png";
 import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
+import { withTranslation, WithTranslation } from "react-i18next";
 
-export const HomePage: React.FC = () => {
-  return (
-    <div>
-      {" "}
-      <Header />
-      {/* 页面内容 */}
-      <div className={styles["page-content"]}>
-        <Row style={{ marginTop: 20 }}>
-          <Col span={6}>
-            <SideMenu />
-          </Col>
-          <Col span={18}>
-            <Carousel />
-          </Col>
-        </Row>
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="warning">
-              爆款推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage}
-          products={productList1}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="danger">
-              新品上市
-            </Typography.Title>
-          }
-          sideImage={sideImage2}
-          products={productList2}
-        />
-        <ProductCollection
-          title={
-            <Typography.Title level={3} type="success">
-              国内游推荐
-            </Typography.Title>
-          }
-          sideImage={sideImage3}
-          products={productList3}
-        />
+class HomePageComponet extends React.Component<WithTranslation> {
+  render() {
+    const { t } = this.props;
+    return (
+      <div>
+        {" "}
+        <Header />
+        {/* 页面内容 */}
+        <div className={styles["page-content"]}>
+          <Row style={{ marginTop: 20 }}>
+            <Col span={6}>
+              <SideMenu />
+            </Col>
+            <Col span={18}>
+              <Carousel />
+            </Col>
+          </Row>
+          <ProductCollection
+            title={
+              <Typography.Title level={3} type="warning">
+                {t("home_page.hot_recommended")}
+              </Typography.Title>
+            }
+            sideImage={sideImage}
+            products={productList1}
+          />
+          <ProductCollection
+            title={
+              <Typography.Title level={3} type="danger">
+                {t("home_page.new_arrival")}
+              </Typography.Title>
+            }
+            sideImage={sideImage2}
+            products={productList2}
+          />
+          <ProductCollection
+            title={
+              <Typography.Title level={3} type="success">
+                {t("home_page.domestic_travel")}
+              </Typography.Title>
+            }
+            sideImage={sideImage3}
+            products={productList3}
+          />
+        </div>
+        <BusinessPartners />
+        <Footer />
       </div>
-      <BusinessPartners />
-      <Footer />
-    </div>
-  );
-};
+    );
+  }
+}
+
+export const HomePage = withTranslation()(HomePageComponet);
